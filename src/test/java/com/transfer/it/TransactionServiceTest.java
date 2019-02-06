d5e9d013a89a2afb8a923d6199f15a82a5a2035d
Bondarchuk, Vladislav <VBondarchuk@nexign-systems.com>
2019-02-06 21:23:38 +0300

    add unit test

--- src/test/java/com/transfer/it/TransactionServiceTest.java
+++ src/test/java/com/transfer/it/TransactionServiceTest.java
@@ -14,23 +14,18 @@ import com.transfer.utils.AppProperties;
 import org.joda.money.CurrencyUnit;
 import org.junit.Before;
 import org.junit.ClassRule;
-import org.junit.Rule;
 import org.junit.Test;
-import org.junit.rules.ExpectedException;
-
-import static org.assertj.core.api.Assertions.*;
-
-import static org.junit.Assert.*;
 
 import java.math.BigDecimal;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.assertj.core.api.Assertions.catchThrowableOfType;
+import static org.junit.Assert.assertEquals;
 
 public class TransactionServiceTest {
 
     @ClassRule
     public static TestContainer cont = TestContainer.Common.INSTANCE;
-
-    @Rule
-    public ExpectedException expectedException = ExpectedException.none();
 
     private AccountService accountService;
     private CustomerService customerService;
@@ -69,6 +64,50 @@ public class TransactionServiceTest {
 
         account1 = accountService.createAccount(account1, savedCustomer.getCustomerId());
         account2 = accountService.createAccount(account2, savedCustomer.getCustomerId());
+
+        UserTransaction transaction = new UserTransaction(new BigDecimal(250000), account1.getAccountNumber(),
+                account2.getAccountNumber());
+
+        transactionService.transferFunds(transaction);
+
+        account1 = accountService.getAccountDetails(account1.getAccountNumber());
+        account2 = accountService.getAccountDetails(account2.getAccountNumber());
+
+        assertEquals(new BigDecimal(250000)
+                .setScale(appProperties.getAppCurrencyScale(), appProperties.getAppRoundingMode()), account1.getBalance());
+        assertEquals(new BigDecimal(750000)
+                .setScale(appProperties.getAppCurrencyScale(), appProperties.getAppRoundingMode()), account2.getBalance());
+    }
+
+    @Test
+    public void testTransferFundsAnotherCustomer() throws Exception {
+        Customer customer1 = new Customer();
+        customer1.setFirstName("TEST_NAME_9");
+        customer1.setLastName("TEST_LAST_NAME_9");
+
+        Customer customer2 = new Customer();
+        customer2.setFirstName("TEST_NAME_10");
+        customer2.setLastName("TEST_LAST_NAME_10");
+
+        Customer savedCustomer1 = customerService.createCustomer(customer1);
+        Customer savedCustomer2 = customerService.createCustomer(customer2);
+
+        Account account1 = new Account();
+        account1.setAccountNumber("401");
+        account1.setType(AccountType.CHECKING.toString());
+        account1.setDescription("Just test account");
+        account1.setCurrencyCode(CurrencyUnit.USD.toString());
+        account1.setBalance(new BigDecimal(500000));
+
+        Account account2 = new Account();
+        account2.setAccountNumber("402");
+        account2.setType(AccountType.SAVINGS.toString());
+        account2.setDescription("Just test account");
+        account2.setCurrencyCode(CurrencyUnit.USD.toString());
+        account2.setBalance(new BigDecimal(500000));
+
+        account1 = accountService.createAccount(account1, savedCustomer1.getCustomerId());
+        account2 = accountService.createAccount(account2, savedCustomer2.getCustomerId());
 
         UserTransaction transaction = new UserTransaction(new BigDecimal(250000), account1.getAccountNumber(),
                 account2.getAccountNumber());
